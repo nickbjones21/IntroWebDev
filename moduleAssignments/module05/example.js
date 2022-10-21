fa4bcd9a7f5860263d0014a28cd295cfc6d2cdda
nickbjones21 <114355037+nickbjones21@users.noreply.github.com>
2022-10-21 16:01:26 -0700

    Update example.js

--- moduleAssignments/module05/example.js
+++ moduleAssignments/module05/example.js
@@ -1,18 +1,9 @@
-var table = 3;             // Unit of table
-var operator = 'addition'; // Type of calculation
+var table = userNum;             // Unit of table
 var i = 1;                 // Set counter to 1
-var msg = '';              // Message
+var msg = '<h2> Multiplication Table <h2>';              // Message
 
-if (operator === 'addition') {
-  // Do addition
   while (i < 11) {
-    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
-    i++;
-  }
-} else {
-  // Do multiplication
-  while (i < 11) {
-    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br>';
     i++;
   }
 }
