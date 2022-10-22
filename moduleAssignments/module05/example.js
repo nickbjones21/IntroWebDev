b357bb07b01bff8fc80b51338e691fe127a10215
nickbjones21 <114355037+nickbjones21@users.noreply.github.com>
2022-10-21 17:09:23 -0700

    Update example.js

--- moduleAssignments/module05/example.js
+++ moduleAssignments/module05/example.js
@@ -1,4 +1,4 @@
-
+function myFunc(){
 var table = prompt("NUMBER HERE", "Enter a number");             // Unit of table
 var i = 1;                 // Set counter to 1
 var msg = '<h2> Multiplication Table <h2>';              // Message
@@ -7,8 +7,9 @@ var msg = '<h2> Multiplication Table <h2>';              // Message
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br>';
     i++;
   }
-}
+
 
 // Write the message into the page
 var el = document.getElementById('blackboard');
 el.innerHTML = msg;
+}
