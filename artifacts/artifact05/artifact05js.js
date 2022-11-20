2e5e74a0b57c0fba3d11ba63eb343daecfeaf81b
nickbjones21 <114355037+nickbjones21@users.noreply.github.com>
2022-11-19 18:17:19 -0800

    Create artifact05js.js

---
+++ artifacts/artifact05/artifact05js.js
@@ -0,0 +1,48 @@
+var myImages =["https://cdn.pixabay.com/photo/2013/07/12/13/21/baseball-146883_960_720.png", 
+"https://cdn.pixabay.com/photo/2019/01/12/20/15/ache-3929184_960_720.png",
+"https://cdn.pixabay.com/photo/2014/09/13/03/23/fire-443549_960_720.png",
+"https://cdn.pixabay.com/photo/2012/04/12/19/36/man-30322_960_720.png",
+"https://cdn.pixabay.com/photo/2017/01/31/14/13/firewood-2024457_960_720.png"];
+
+
+var captionImages =["Baseball","Backpain","Fire","Trash","Log"];
+
+ var index=0; 
+
+ function updateImage(){
+ document.getElementById("slideshow").src = myImages[index];
+ document.getElementById("slideshow").alt= captionImages[index];
+ document.getElementById("caption").textContent = captionImages[index]; 
+} 
+
+function next(){
+ if (myImages.length == index+1)
+ index=0;
+ else
+ index++;
+ updateImage();
+} 
+ 
+
+function back(){
+ if (index===0)
+ index=myImages.length-1;
+ else
+ index--;
+ 
+ updateImage();
+} 
+
+var nextButton = document.getElementById("next"); 
+var previousButton = document.getElementById("previous"); 
+
+previousButton.addEventListener("click",back,false);
+nextButton.addEventListener("click",next,false); 
+
+function autoSlide(){
+if (document.getElementById("auto").checked)
+ next(); 
+}
+
+
+setInterval(autoSlide,2000); // Next
